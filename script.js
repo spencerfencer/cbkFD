55d77628fa3491d921ea9a9af8f17d8db7fcb6f2
spencerfencer <130942687+spencerfencer@users.noreply.github.com>
2024-06-28 19:23:40 +0000

    started files

---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("joinForm");
+    const submitButton = form.querySelector("button[type='submit']");
+    const spinner = document.getElementById("spinner");
+    const checkmark = document.querySelector(".checkmark");
+    const hero = document.querySelector(".hero");
+    const scriptURL = "https://script.google.com/macros/s/AKfycbxcqal6ygl-nHY--pJvwG7P-J4q4100ws7N8NEB8iB5wN52y9815YHo1xOGomRAv90m/exec";
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        submitButton.disabled = true;
+        hero.classList.add("loading");
+        spinner.style.display = "block";
+
+        let requestBody = new FormData(form);
+        fetch(scriptURL, { method: "POST", body: requestBody })
+            .then(response => {
+                hero.classList.remove("loading");
+                hero.classList.add("success");
+                submitButton.disabled = false;
+                form.reset();
+
+                checkmark.style.display = "block";
+                spinner.style.display = "none";
+            })
+            .catch(error => {
+                console.error("Error:", error.message);
+                hero.classList.remove("loading");
+                submitButton.disabled = false;
+                spinner.style.display = "none";
+            });
+    });
+});
+
+
+
